3985d1627ed234929abd70ae71dd58f6f8bd5447
Hayden Ong <haydenhcong@gmail.com>
2024-08-10 17:05:31 +0800

    10/8/2024 Version

---
+++ HTML/JS/app.js
@@ -0,0 +1,36 @@
+function toggleDropdown() {
+  const dropdownMenu = document.getElementById("navbar_menu");
+  dropdownMenu.classList.toggle("active");
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  const dropDownItems = document.querySelectorAll(".dropDown");
+
+
+  dropDownItems.forEach(function(dropDownItem) {
+      dropDownItem.addEventListener("click", function() {
+          const submenu = this.querySelector("ul");
+
+          if (submenu) {
+              const dropItems = submenu.querySelectorAll(".drop_item");
+
+              dropItems.forEach(function(dropItem) {
+                  if (dropItem.style.display === "none" || dropItem.style.display === "") {
+                      dropItem.style.display = "flex";
+                      
+                  } else {
+                      dropItem.style.display = "none";
+                  }
+              });
+          }
+      });
+  });
+});
+
+
+//return to top button
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
